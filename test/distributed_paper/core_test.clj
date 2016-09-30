c7ad6728ed5af645a41328a3f3e89ab097ccff3b
akoti <akoti@fanatics.com>
2016-09-29 17:04:48 -0700

    Initial Checkin

--- test/distributed_paper/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns distributed-paper.core-test
-  (:require [clojure.test :refer :all]
-            [distributed-paper.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
--- test/doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to distributed-paper
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
